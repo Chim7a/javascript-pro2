86d55d8f9e2904a017cd3183f1f33d2770ebb3a1
chim7a <chimalawrence11@gmail.com>
2024-09-24 22:03:54 +0100

    cap first words

--- script.js
+++ script.js
@@ -1,14 +1,12 @@
+// String Functions
+
 // Reverse a String
 
 function reverseString(stringInput) {
   let stringReverse = stringInput.split("");
-  console.log(stringReverse);
   let reversedText = stringReverse.reverse();
-  console.log(reversedText);
 
   return reversedText.join("");
-
-  // return stringInput.split("").reverse().join("");
 }
 
 console.log(reverseString("amihC"));
@@ -20,3 +18,30 @@ const countString = (stringInput) => {
 };
 
 console.log(countString("Chima Ikegbulam"));
+
+// Capitalize first letters of sentence
+
+function capitalizeFistWord(textInput) {
+  const mySentence = textInput;
+  const words = mySentence.split(" ");
+
+  const newSentence = words.map((word) => {
+    let upperCaseSentence = word[0].toUpperCase() + word.substring(1);
+    return upperCaseSentence;
+  });
+
+  return newSentence.join(" ");
+}
+
+console.log(capitalizeFistWord("chima paul is a big man"));
+
+// Array Functions
+
+// Find Maximum and Minimum
+// const christmasEve = new Date("December 24, 1975 23:15:30");
+// const date1 = christmasEve.getDate() + " " + christmasEve.getMonth();
+// console.log(date1);
+
+//
+
+const ages = [2, 22, 23, 3, 2, 4, 6];
